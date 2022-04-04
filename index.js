b1c9f899b93c243c11ba7a8b977b989ee15ef11e
Kaitlin Shumake <shumakek@ufl.edu>
2022-04-03 20:55:55 -0400

    fixing errors

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ var textArea = document.querySelector('textarea')
 // Retrieve name and note content from cookies and localstorage
 // Then apply them to elements on the page
 // YOUR CODE HERE
-newTitle = document.cookie;
-nameSpan.textContent = newTitle;
+
+  var title = document.cookie
+  nameSpan.textContent = title
+
 
 
 textArea.value = localStorage.getItem('textarea');
@@ -20,23 +22,26 @@ formEl.onsubmit = function(e) {
   e.preventDefault();
   // save name element's content to cookies
   // save textarea's content to localstorage
-    localStorage.setItem('textarea', textArea.value);
-    document.cookie = nameSpan.textContent;
+    localStorage.setItem('textarea', textArea.value)
+    document.cookie = nameSpan.textContent
+    nameSpan.textContent = title;
 
   
   // triggers thumbs up animation
-  this.elements.save.classList.add('emoji');
+  this.elements.save.classList.add('emoji')
 }
 
 clear.onclick = function() {
   // Clear textarea's value
   // Clear localstorage's content
   // YOUR CODE HERE
-  textArea.innerHTML = " ";
-  localStorage.clear();
+  textArea.value = " "
+  localStorage.clear()
+
   // triggers thumbs up animation
-  this.classList.add('emoji');
+  this.classList.add('emoji')
 }
+
 
 // this code allows repeated thumbs up animations
 function endThumbsUp() {
